e44d1152dc8c8f862582a5a5946d23e8fc661356
Jack <jhkent88@gmail.com>
2018-09-13 16:32:23 -0700

    Begin tutoring session

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var express = require('express');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// routes
+var routes = require("./controllers/burger_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
